6efd36ed8ca4a9beebccfa439e8c5c5842137afd
Lucienne <soarimalalahantamarielucienne@gmail.com>
2026-09-09 13:51:50 +0300

    test build casse pour verifier automatisation

--- Backend/src/main/java/com/example/demo/service/PatientService.java
+++ Backend/src/main/java/com/example/demo/service/PatientService.java
@@ -27,7 +27,7 @@ public class PatientService {
         return patientRepository.save(patient);
     }
 
-    /*public Patient update(String codepat, Patient patientDetails) {
+    public Patient update(String codepat, Patient patientDetails) {
         Patient patient = getById(codepat);
         patient.setNom(patientDetails.getNom());
         patient.setPrenom(patientDetails.getPrenom());
@@ -35,7 +35,7 @@ public class PatientService {
         patient.setAdresse(patientDetails.getAdresse());
         return patientRepository.save(patient);
     }
-*/
+
     public void delete(String codepat) {
         patientRepository.deleteById(codepat);
     }
