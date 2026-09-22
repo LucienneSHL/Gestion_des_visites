2beb371e323802f1d71c1473e3e07d772774d00c
Lucienne <soarimalalahantamarielucienne@gmail.com>
2026-09-22 16:49:48 +0300

    Test deploiement automatique

--- Backend/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/src/main/java/com/example/demo/DemoApplication.java
@@ -8,5 +8,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        // Test deploiement automatique - 22/09/2026
     }
 }
